cd4107cbf9fbb698ee9e3547903a7929a4db64bd
Diego Flores Daniel <dfd@Diegos-MacBook-Pro.local>
2026-05-01 15:16:45 -0500

    feat: make animated underline persist after clicking until focus is lost

--- src/components/ui/animated-underline.tsx
+++ src/components/ui/animated-underline.tsx
@@ -13,11 +13,14 @@ export const AnimatedUnderline = ({
   underlineColor?: string;
 }) => {
   return (
-    <span className={cn(
-      "relative inline-block after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300", 
-      underlineColor,
-      className
-    )}>
+    <span 
+      tabIndex={0}
+      className={cn(
+        "relative inline-block after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 focus:outline-none focus:after:origin-bottom-left focus:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 cursor-pointer", 
+        underlineColor,
+        className
+      )}
+    >
       {children}
     </span>
   );
