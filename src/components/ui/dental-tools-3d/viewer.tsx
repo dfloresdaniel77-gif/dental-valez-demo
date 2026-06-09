5f8c84a5f1edb83a36c0dc7b61bb712732ab027e
Diego Flores Daniel <dfd@Diegos-MacBook-Pro.local>
2026-06-09 16:49:03 -0500

    feat: move tray and tools to Y=0 to perfectly frame the text as requested

--- src/components/ui/dental-tools-3d/viewer.tsx
+++ src/components/ui/dental-tools-3d/viewer.tsx
@@ -13,7 +13,7 @@ const TOOL_ANIMATIONS = [
     // Mirror
     startPos: new THREE.Vector3(-2.0, 2.4, -1),
     startRot: new THREE.Euler(Math.PI / 4, Math.PI, Math.PI / 3),
-    endPos: new THREE.Vector3(-1.2, -0.75, 0),
+    endPos: new THREE.Vector3(-1.2, 0.05, 0),
     endRot: new THREE.Euler(-Math.PI / 2 + 0.15, 0, 0),
     scrollRange: [0.0, 0.20],
   },
@@ -21,7 +21,7 @@ const TOOL_ANIMATIONS = [
     // Scaler
     startPos: new THREE.Vector3(2.0, 2.4, 1),
     startRot: new THREE.Euler(-Math.PI / 3, Math.PI / 2, -Math.PI / 4),
-    endPos: new THREE.Vector3(-0.6, -0.75, 0),
+    endPos: new THREE.Vector3(-0.6, 0.05, 0),
     endRot: new THREE.Euler(-Math.PI / 2 + 0.15, 0, 0),
     scrollRange: [0.15, 0.35],
   },
@@ -29,7 +29,7 @@ const TOOL_ANIMATIONS = [
     // Probe
     startPos: new THREE.Vector3(0, 2.4, -2),
     startRot: new THREE.Euler(Math.PI / 2, -Math.PI / 4, Math.PI / 6),
-    endPos: new THREE.Vector3(0, -0.75, 0),
+    endPos: new THREE.Vector3(0, 0.05, 0),
     endRot: new THREE.Euler(-Math.PI / 2 + 0.15, 0, 0),
     scrollRange: [0.30, 0.50],
   },
@@ -37,7 +37,7 @@ const TOOL_ANIMATIONS = [
     // Syringe
     startPos: new THREE.Vector3(-2.2, 0.5, 1),
     startRot: new THREE.Euler(-Math.PI / 6, Math.PI / 3, -Math.PI / 2),
-    endPos: new THREE.Vector3(0.6, -0.75, 0),
+    endPos: new THREE.Vector3(0.6, 0.05, 0),
     endRot: new THREE.Euler(-Math.PI / 2 + 0.15, 0, 0),
     scrollRange: [0.45, 0.65],
   },
@@ -45,7 +45,7 @@ const TOOL_ANIMATIONS = [
     // Forceps
     startPos: new THREE.Vector3(2.2, 0.2, 2),
     startRot: new THREE.Euler(Math.PI / 3, -Math.PI / 6, Math.PI / 4),
-    endPos: new THREE.Vector3(1.2, -0.75, 0),
+    endPos: new THREE.Vector3(1.2, 0.05, 0),
     endRot: new THREE.Euler(-Math.PI / 2 + 0.15, 0, 0),
     scrollRange: [0.60, 0.82],
   },
@@ -96,7 +96,7 @@ function SceneAnimator({ scrollYProgress }: { scrollYProgress: MotionValue<numbe
   return (
     <>
       {/* The Tray is static at the bottom */}
-      <group position={[0, -0.8, 0]} rotation={[0.15, 0, 0]}>
+      <group position={[0, 0, 0]} rotation={[0.15, 0, 0]}>
         <SurgicalTray />
       </group>
 
@@ -142,7 +142,7 @@ export function ToolViewer3D({ scrollYProgress }: { scrollYProgress: MotionValue
         <SceneAnimator scrollYProgress={scrollYProgress} />
 
         {/* Realistic ground shadow below the tray */}
-        <ContactShadows position={[0, -0.95, 0]} opacity={0.4} scale={10} blur={2} far={4} />
+        <ContactShadows position={[0, -0.15, 0]} opacity={0.4} scale={10} blur={2} far={4} />
       </Suspense>
     </Canvas>
   );
